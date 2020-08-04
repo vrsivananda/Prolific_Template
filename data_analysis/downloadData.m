0ea789d7f63a84a0dfc830c8c65875048098d8c4
vrsivananda <vrsivananda@gmail.com>
2020-08-03 22:44:48 -0700

    Fixed PROLIFIC_ID in downloadData.m

--- data_analysis/downloadData.m
+++ data_analysis/downloadData.m
@@ -48,7 +48,7 @@ for i = 1:numberOfSubjects
     fprintf('subject: %s\n',subjectId);
 
     % Make the SQL query to download everything.
-    sqlquery = ['SELECT * FROM ' tableName ' WHERE workerId = "' subjectId '"'];
+    sqlquery = ['SELECT * FROM ' tableName ' WHERE PROLIFIC_PID = "' subjectId '"'];
     
     % Change the path
     currentFolderPath = pwd;
